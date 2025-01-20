d87ff345ee7353d1e8b09834ed4d3808373bf1da
YennieMoralesRoco <yennie98mr@live.cl>
2025-01-20 20:35:21 -0300

    Modificación administrador

--- AppConsulta/admin.py
+++ AppConsulta/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Paciente, Medico
+from .models import Paciente, Medico, Consulta
 
 # Register your models here.
 
 admin.site.register(Paciente)
 admin.site.register(Medico)
+admin.site.register(Consulta)
